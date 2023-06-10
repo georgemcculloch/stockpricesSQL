29e13b12adac8fb3083f858851e3071bf94072c2
George McCulloch <georgehenrymcculloch@gmail.com>
2023-06-10 22:18:05 +0100

    did some things

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# stockpricesSQL
+### **Stock Analysis**
+---
--- stocks.sql
+++ stocks.sql
@@ -38,7 +38,6 @@ CREATE TABLE student.gm_msft(
     Name VARCHAR
 );
 
-
 SELECT
     *
 FROM gm_aapl;
@@ -54,3 +53,37 @@ FROM gm_goog;
 SELECT
     *
 FROM gm_msft;
+
+-- ANSWER THESE FOR EACH COMPANY
+-- average daily volume?
+-- highest closing price?
+-- something with some maths
+-- something HAVING something CONCAT something SUBSTRING something
+-- GROUP BY aggregate something
+
+-- THEN ANSWER THESE WITH JOINS
+-- which company had the highest open/close price on set date
+-- average volume comparison by name on set date
+-- average daily range
+-- percentage change im tired ill word this better later
+
+
+-- apple
+/* insert amazing(ly average) code here */
+
+
+-- amazon
+/* insert amazing code here */
+
+
+-- google
+/* insert amazing code here */
+
+
+-- microsoft
+/* insert amazing code here */
+
+
+-- joins and comparisons
+/* insert amazing code here */
+
