9098d174cd25b652991e3ff0fcf6652a1a9f200c
George McCulloch <georgehenrymcculloch@gmail.com>
2023-06-11 09:36:38 +0100

    outliers query, apple queries

--- stocks.sql
+++ stocks.sql
@@ -67,10 +67,99 @@ FROM gm_msft;
 -- average daily range
 -- percentage change im tired ill word this better later
 
+SELECT
+    min(date) as min_date,
+    max(date) as max_date,
+    min(open) as min_open,
+    max(open) as max_open,
+    min(high) as min_high,
+    max(high) as max_high,
+    min(low) as min_low,
+    max(low) as max_low,
+    min(close) as min_close,
+    max(close) as max_close,
+    min(volume) as min_volume,
+    max(volume) as max_volume,
+    name
+FROM (
+    SELECT
+        name,
+        date,
+        open,
+        high,
+        low,
+        close,
+        volume
+    FROM gm_aapl
+    UNION ALL
+    SELECT
+        name,
+        date,
+        open,
+        high,
+        low,
+        close,
+        volume
+    FROM gm_amzn
+    UNION ALL
+    SELECT
+        name,
+        date,
+        open,
+        high,
+        low,
+        close,
+        volume
+    FROM gm_goog
+    UNION ALL
+    SELECT
+        name,
+        date,
+        open,
+        high,
+        low,
+        close,
+        volume
+    FROM gm_msft
+     ) as date_tables
+GROUP BY
+    name;
+
+-- insightful insights
 
 -- apple
 /* insert amazing(ly average) code here */
+-- average daily volume and average volume all time
+SELECT
+    avg(volume) as average_daily_volume,
+    date,
+    name
+FROM
+    gm_aapl
+GROUP BY
+    date,
+    name;
 
+SELECT
+    round(avg(volume), 2) as average_volume_all_time,
+    name
+FROM
+    gm_aapl
+GROUP BY
+    name;
+
+-- highest closing price
+
+SELECT
+    max(close) as highest_closing_price,
+    name,
+    date
+FROM
+    gm_aapl
+GROUP BY
+    name,
+    date
+LIMIT 1;
 
 -- amazon
 /* insert amazing code here */
